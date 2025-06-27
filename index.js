cd0df5d706d784f9706172c309e5ac7c59a3effc
Yurdoy <yurdoy@gmail.com>
2025-06-27 11:15:20 +0200

    Tasks 1 - 3 completed.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.send("World... hold on");
 });
 
+app.use((err, req, res, next) => {
+  console.error("Error during request handling", err);
+  res.status(500).send("Internal server error");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
