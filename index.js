f1e1a36f9f1ea6a50f9d741a430fb7b0effeb272
Yurdoy <yurdoy@gmail.com>
2025-06-27 12:21:36 +0200

    Task 3 updated: Object.key() function added for checking that object has keys

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("World... hold on");
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  try {
+    res.send("World... hold on");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/", (req, res) => {
+  try {
+    const data = req.body;
+    if (!data || Object.keys(data).length === 0) {
+      return res.status(400).json({ error: "No data provided" });
+    }
+    res.status(200).json({ message: "Data received", data });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use((err, req, res, next) => {
